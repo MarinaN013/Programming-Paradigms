7d49f1218cb05c6ad9573a6e2aa001915c8c8ea9
ВАШЕ Имя <marina013@bk.ru>
2023-12-05 10:01:42 +0300

    Solution homework 5 procedural paradigm

--- src/HomeWork_5/Main.java
+++ src/HomeWork_5/Main.java
@@ -1,7 +1,11 @@
 package HomeWork_5;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        int[] array = {1, 2, 3, 4};
+        int sum = Arrays.stream(array).sum();//преобразовываем массив в поток целых чисел
+        System.out.println(sum);
     }
 }
